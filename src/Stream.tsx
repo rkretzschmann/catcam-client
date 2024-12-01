f71a902f5f57e207668658d4382c6440b575667a
Roman Kretzschmann <kretzschmann@mailbox.org>
2024-12-01 20:42:28 +0100

    Autoconnect

--- src/Stream.tsx
+++ src/Stream.tsx
@@ -1,9 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 import FullscreenButton from "./components/FullscreenButton";
+import { Backdrop, CircularProgress } from "@mui/material";
 
 const Stream = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [isContentLoaded, setIsContentLoaded] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [retryCount, setRetryCount] = useState(0);
+  const [streamUrl, setStreamUrl] = useState(
+    "http://192.168.178.125:7123/stream.mjpg"
+  );
+
+  useEffect(() => {
+    if (hasError && retryCount < 5000) {
+      const retryTimeout = setTimeout(() => {
+        setRetryCount((prev) => prev + 1);
+        setHasError(false); // Reset error state to retry loading
+        // Change the stream URL to trigger a reload
+        setStreamUrl(
+          `http://192.168.178.125:7123/stream.mjpg?retry=${Date.now()}`
+        );
+      }, 3000);
+      return () => clearTimeout(retryTimeout); // Clear timeout if component unmounts
+    }
+  }, [hasError, retryCount]);
+
+  // Handle the image loading error
+  const handleError = () => {
+    setHasError(true); // Set error state when the stream fails to load
+  };
 
   const centerScrollbar = () => {
     if (containerRef.current) {
@@ -36,10 +61,20 @@ const Stream = () => {
         alignItems: "center",
       }}
     >
+      {!isContentLoaded ? (
+        <Backdrop open sx={{ backgroundColor: "black" }}>
+          <CircularProgress />
+        </Backdrop>
+      ) : null}
+
       <img
-        src="http://192.168.178.125:7123/stream.mjpg"
+        src={streamUrl}
         alt="Video Stream"
-        onLoad={() => setIsContentLoaded(true)}
+        onLoad={() => {
+          console.log("Loaded");
+          setIsContentLoaded(true);
+        }} // Set content loaded state
+        onError={handleError} // Set error state if image fails to load
         style={{
           height: "100%",
           width: "auto",
